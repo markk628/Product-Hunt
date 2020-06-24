747fc365d3cdd858fc85a136e71402dc57c58ac7
Donghyun <markk628@gmail.com>
2020-06-23 22:52:31 -0700

    Created Post model

---
+++ ProductHuntApp/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  ProductHuntApp
+//
+//  Created by Mark Kim on 6/23/20.
+//  Copyright © 2020 Mark Kim. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
